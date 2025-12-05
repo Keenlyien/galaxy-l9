52402b1e1ca93c5f759d87bdd80ac92c3fd33965
Keenlyien <137278669+Keenlyien@users.noreply.github.com>
2025-12-05 14:13:42 +0800

    Update sseController.js

--- api/sseController.js
+++ api/sseController.js
@@ -1,1 +1,22 @@
+let clients = [];
 
+
+export function getSSEController() {
+return {
+addClient(res) {
+clients.push(res);
+res.write(`event: connected\n`);
+res.write(`data: connected\n\n`);
+
+
+req.on("close", () => {
+clients = clients.filter((c) => c !== res);
+});
+},
+
+
+broadcast(data) {
+const payload = `data: ${JSON.stringify(data)}\n\n`;
+clients.forEach((res) => res.write(payload));
+},
+};
